ed531caae756555f6a0282e1c398255aafc2aef8
Vishnu Bharathi <vishnubharathi04@gmail.com>
2016-11-13 17:11:06 +0530

    adds feature list prototype

---
+++ public/app.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    name : 'Vishnu',
+    features : [
+      {
+        title: 'feature 1',
+        description: 'Hello world feature'
+      },
+      {
+        title: 'feature 2',
+        description: 'User Addition feature'
+      }
+    ]
+  },
+  methods: {
+
+  }
+})
